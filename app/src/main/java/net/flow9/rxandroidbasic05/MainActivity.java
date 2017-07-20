87dbf5ccf79cf79d35937ac969d551e4b6fa314c
javafa <nigne2016@gmail.com>
2017-07-20 11:22:05 +0900

    init

--- app/src/main/java/net/flow9/rxandroidbasic05/MainActivity.java
+++ app/src/main/java/net/flow9/rxandroidbasic05/MainActivity.java
@@ -5,10 +5,18 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 
+import java.util.concurrent.TimeUnit;
+
+import io.reactivex.Observable;
 import io.reactivex.Scheduler;
 import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.schedulers.Schedulers;
+import io.reactivex.subjects.AsyncSubject;
+import io.reactivex.subjects.BehaviorSubject;
 import io.reactivex.subjects.PublishSubject;
+import io.reactivex.subjects.ReplaySubject;
+
+import static java.lang.Thread.sleep;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -23,9 +31,9 @@ public class MainActivity extends AppCompatActivity {
     public void doPublish(View view){
         new Thread() {
             public void run() {
-                for (int i = 0; i < 100; i++) {
-                    publishSubject.onNext("A" + i); // 발행
+                for (int i = 0; i < 10; i++) {
                     Log.i("Publish", "A" + i);
+                    publishSubject.onNext("A" + i); // 발행
                     try {
                         Thread.sleep(1000);
                     } catch (InterruptedException e) {/* */}
@@ -39,16 +47,60 @@ public class MainActivity extends AppCompatActivity {
                 item -> Log.i("Subscribe", "item="+item)
         );
     }
-
+    //------------------------------------------------------
+    BehaviorSubject<String> behaviorSubject = BehaviorSubject.create();
+    // 발행
     public void doBehavior(View view){
-
+        new Thread() {
+            public void run() {
+                for (int i = 0; i < 10; i++) {
+                    Log.i("Behavior", "B" + i);
+                    behaviorSubject.onNext("B" + i); // 발행
+                    try { Thread.sleep(1000); } catch (InterruptedException e) {/* */}
+                }
+            }
+        }.start();
     }
-
+    public void getBehavior(View view){
+        behaviorSubject.subscribe(
+                item -> Log.i("Behavior", "item="+item)
+        );
+    }
+    //------------------------------------------------------
+    ReplaySubject<String> replaySubject = ReplaySubject.create();
     public void doReplay(View view){
-
+        new Thread() {
+            public void run() {
+                for (int i = 0; i < 10; i++) {
+                    Log.i("Replay", "C" + i);
+                    replaySubject.onNext("C" + i); // 발행
+                    try { Thread.sleep(1000); } catch (InterruptedException e) {/* */}
+                }
+            }
+        }.start();
     }
-
+    public void getReplay(View view){
+        replaySubject.subscribe(
+                item -> Log.i("Replay", "item="+item)
+        );
+    }
+    //------------------------------------------------------
+    AsyncSubject<String> asyncSubject = AsyncSubject.create();
     public void doAsync(View view){
-
+        new Thread() {
+            public void run() {
+                for (int i = 0; i < 10; i++) {
+                    Log.i("Async", "D" + i);
+                    asyncSubject.onNext("D" + i); // 발행
+                    try { Thread.sleep(1000); } catch (InterruptedException e) {/* */}
+                }
+                asyncSubject.onComplete();
+            }
+        }.start();
+    }
+    public void getAsync(View view){
+        asyncSubject.subscribe(
+                item -> Log.i("Replay", "item="+item)
+        );
     }
 }
